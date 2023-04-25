a327fec173659fd76c6f510893c7f74a9fd73787
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2023-04-25 16:35:24 +0300

    Update main.test.js

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,32 +1,31 @@
-const { displayResult } = require('../main.js');
+const { displayResult } = require('./main');
 
-describe('displayResult function', () => {
-  test('should update the DOM with the correct weather information', () => {
-    const weatherData = {
-      name: 'Test City',
-      sys: { country: 'Test Country' },
-      main: { temp: 10, temp_min: 5, temp_max: 15 },
-      weather: [{ main: 'Test Weather' }]
-    };
+test('displayResult function formats weather data correctly', () => {
+  const weatherData = {
+    name: 'London',
+    sys: { country: 'UK' },
+    main: { temp: 10.5, temp_min: 9.0, temp_max: 12.0 },
+    weather: [{ main: 'Cloudy' }]
+  };
 
-    document.body.innerHTML = `
-      <div class="location">
-        <div class="city"></div>
-        <div class="date"></div>
-      </div>
-      <div class="current">
-        <div class="temp"></div>
-        <div class="weather"></div>
-      </div>
-      <div class="hi-low"></div>
-    `;
+  document.body.innerHTML = `
+    <div class="location">
+      <div class="city"></div>
+      <div class="date"></div>
+    </div>
+    <div class="current">
+      <div class="temp"></div>
+      <div class="weather"></div>
+    </div>
+    <div class="hi-low"></div>
+  `;
 
-    displayResult(weatherData);
+  displayResult(weatherData);
 
-    expect(document.querySelector('.location .city').innerText).toBe('Test City, Test Country');
-    expect(document.querySelector('.location .date').innerText).toMatch(/[a-zA-Z]+ \d{1,2} [a-zA-Z]+ \d{4}/);
-    expect(document.querySelector('.current .temp').innerHTML).toBe('10<span>°c</span>');
-    expect(document.querySelector('.current .weather').innerText).toBe('Test Weather');
-    expect(document.querySelector('.hi-low').innerText).toBe('5°c/15°c');
-  });
+  expect(document.querySelector('.location .city').textContent).toBe('London, UK');
+  expect(document.querySelector('.location .date').textContent).toBeTruthy();
+  expect(document.querySelector('.current .temp').innerHTML).toBe('11<span>°c</span>');
+  expect(document.querySelector('.current .weather').textContent).toBe('Cloudy');
+  expect(document.querySelector('.hi-low').textContent).toBe('9°c/12°c');
 });
+
