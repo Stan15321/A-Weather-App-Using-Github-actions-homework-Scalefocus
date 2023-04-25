cd6f4c1d89cca15a08f46ff3f59a4a30ee227508
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2023-04-25 17:49:03 +0300

    update

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,17 +1,5 @@
-const { JSDOM } = require('jsdom');
 
-const { displayResult } = require('../main');
 
-test('displayResult function formats weather data correctly', () => {
-  const dom = new JSDOM('<html><body></body></html>');
-  global.window = dom.window;
-  global.document = dom.window.document;
-
-  const container = document.createElement('div');
-  displayResult({ weather: { temp: 25 }, name: 'London' }, container);
-
-  expect(container.innerHTML).toBe('<h2>London</h2><p>Temperature: 25°C</p>');
-});
 
 
 
